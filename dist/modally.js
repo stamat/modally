3edac1fa1a4f541e577464338da7fa15ba3acc1d
Nikola Stamatovic <stamatmail@gmail.com>
2023-08-07 00:56:21 +0200

    new modal open and modal close functions

--- dist/modally.js
+++ dist/modally.js
@@ -6,6 +6,25 @@
       target[key] = source[key];
     }
   }
+  function isEmptyObject(o) {
+    for (const i in o) {
+      return false;
+    }
+    return true;
+  }
+  function isEmptyArray(o) {
+    return o.length === 0;
+  }
+  function isEmpty(o) {
+    if (isObject(o)) {
+      return isEmptyObject(o);
+    } else if (isArray(o)) {
+      return isEmptyArray(o);
+    } else if (isString(o)) {
+      return o === "";
+    }
+    return false;
+  }
   function stringToBoolean(str) {
     if (/^\s*(true|false)\s*$/i.test(str))
       return str === "true";
@@ -47,6 +66,12 @@
     if (bool !== void 0)
       return bool;
     return stringToNumber(str) || stringToArray(str) || stringToObject(str) || stringToRegex(str) || str;
+  }
+  function isObject(o) {
+    return typeof o === "object" && !Array.isArray(o) && o !== null;
+  }
+  function isArray(o) {
+    return Array.isArray(o);
   }
   function isString(o) {
     return typeof o === "string";
@@ -97,15 +122,227 @@
     return scrollbarWidth;
   }
 
+  // ../book-of-spells/src/helpers.mjs
+  function isEmptyObject2(o) {
+    for (const i in o) {
+      return false;
+    }
+    return true;
+  }
+  function isEmptyArray2(o) {
+    return o.length === 0;
+  }
+  function isEmpty2(o) {
+    if (isObject2(o)) {
+      return isEmptyObject2(o);
+    } else if (isArray2(o)) {
+      return isEmptyArray2(o);
+    } else if (isString2(o)) {
+      return o === "";
+    }
+    return false;
+  }
+  function stringToBoolean2(str) {
+    if (/^\s*(true|false)\s*$/i.test(str))
+      return str === "true";
+  }
+  function stringToNumber2(str) {
+    if (/^\s*\d+\s*$/.test(str))
+      return parseInt(str);
+    if (/^\s*[\d.]+\s*$/.test(str))
+      return parseFloat(str);
+  }
+  function stringToArray2(str) {
+    if (!/^\s*\[.*\]\s*$/.test(str))
+      return;
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+    }
+  }
+  function stringToObject2(str) {
+    if (!/^\s*\{.*\}\s*$/.test(str))
+      return;
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+    }
+  }
+  function stringToRegex2(str) {
+    if (!/^\s*\/.*\/g?i?\s*$/.test(str))
+      return;
+    try {
+      return new RegExp(str);
+    } catch (e) {
+    }
+  }
+  function stringToType2(str) {
+    if (/^\s*null\s*$/.test(str))
+      return null;
+    const bool = stringToBoolean2(str);
+    if (bool !== void 0)
+      return bool;
+    return stringToNumber2(str) || stringToArray2(str) || stringToObject2(str) || stringToRegex2(str) || str;
+  }
+  function isObject2(o) {
+    return typeof o === "object" && !Array.isArray(o) && o !== null;
+  }
+  function isArray2(o) {
+    return Array.isArray(o);
+  }
+  function isString2(o) {
+    return typeof o === "string";
+  }
+  function isFunction2(o) {
+    return typeof o === "function";
+  }
+
+  // ../book-of-spells/src/dom.mjs
+  function cssTimeToMilliseconds(duration) {
+    const regExp = new RegExp("([0-9.]+)([a-z]+)", "i");
+    const matches = regExp.exec(duration);
+    if (!matches)
+      return 0;
+    const unit = matches[2];
+    switch (unit) {
+      case "ms":
+        return parseFloat(matches[1]);
+      case "s":
+        return parseFloat(matches[1]) * 1e3;
+      default:
+        return 0;
+    }
+  }
+  function getTransitionDurations2(element) {
+    if (!element) {
+    }
+    const styles = getComputedStyle(element);
+    const transitionProperties = styles.getPropertyValue("transition-property").split(",");
+    const transitionDurations = styles.getPropertyValue("transition-duration").split(",");
+    const map = {};
+    for (let i = 0; i < transitionProperties.length; i++) {
+      const property = transitionProperties[i].trim();
+      map[property] = transitionDurations.hasOwnProperty(i) ? cssTimeToMilliseconds(transitionDurations[i].trim()) : null;
+    }
+    return map;
+  }
+
+  // ../book-of-spells/src/parsers.mjs
+  function parseUrlParameters(paramString, decode = true) {
+    const res = {};
+    const paramParts = paramString.split("&");
+    paramParts.forEach((part) => {
+      const m = part.match(/([^\s=&]+)=?([^&\s]+)?/);
+      const key = m[1];
+      const value = m[2];
+      res[key] = value !== void 0 && decode ? stringToType2(decodeURIComponent(value)) : value;
+    });
+    return res;
+  }
+
+  // ../book-of-spells/src/browser.mjs
+  function getHashProperties(entryHash) {
+    const hash = entryHash ? entryHash : window.location.hash.replace("#", "");
+    if (isEmpty2(hash))
+      return {};
+    return parseUrlParameters(hash);
+  }
+  function onHashChange(callback) {
+    const hash = window.location.hash.replace("#", "");
+    if (!isEmpty2(hash))
+      callback(hash);
+  }
+  function hashChange(callback) {
+    onHashChange(callback);
+    window.addEventListener("hashchange", () => {
+      onHashChange(callback);
+    });
+  }
+
+  // ../book-of-spells/src/animations.mjs
+  function clearTransitionTimer(element, property = "all") {
+    if (!element)
+      return;
+    const dataPropName = `${property}TransitionTimer`;
+    if (!element.dataset[dataPropName])
+      return;
+    clearTimeout(parseInt(element.dataset[dataPropName]));
+    delete element.dataset[dataPropName];
+  }
+  function setTransitionTimer(element, property = "all", timeout, callback) {
+    if (!element)
+      return;
+    const dataPropName = `${property}TransitionTimer`;
+    const timer = setTimeout(() => {
+      clearTransitionTimer(element, property);
+      if (isFunction2(callback))
+        callback(element);
+    }, timeout);
+    element.dataset[dataPropName] = timer.toString();
+    return timer;
+  }
+  function setTransitionDuration(element, property = "all") {
+    if (!element)
+      return;
+    const dataPropName = `${property}TransitionDuration`;
+    if (element.dataset[dataPropName])
+      return parseInt(element.dataset[dataPropName]);
+    const transitionDurations = getTransitionDurations2(element);
+    if (!transitionDurations.hasOwnProperty(property))
+      return;
+    element.dataset[dataPropName] = transitionDurations[property].toString();
+    return transitionDurations[property];
+  }
+  function fadeIn(element, callback) {
+    if (!element)
+      return;
+    clearTransitionTimer(element, "opacity");
+    const styles = getComputedStyle(element);
+    const duration = setTransitionDuration(element, "opacity");
+    let oldOpacity = parseInt(styles.opacity);
+    if (isNaN(oldOpacity))
+      oldOpacity = 0;
+    if (element.hasAttribute("hidden"))
+      element.removeAttribute("hidden");
+    element.style.pointerEvents = "none";
+    if (!oldOpacity)
+      element.style.visibility = "hidden";
+    element.style.display = "block";
+    element.style.opacity = oldOpacity ? oldOpacity : 0;
+    setTimeout(() => {
+      element.style.opacity = 1;
+      element.style.visibility = "visible";
+      element.style.removeProperty("pointer-events");
+    }, 10);
+    setTransitionTimer(element, "opacity", duration, (element2) => {
+      if (isFunction2(callback))
+        callback(element2);
+    });
+  }
+  function fadeOut(element, callback) {
+    if (!element)
+      return;
+    clearTransitionTimer(element, "opacity");
+    const styles = getComputedStyle(element);
+    const duration = setTransitionDuration(element, "opacity");
+    element.style.opacity = styles.opacity;
+    setTimeout(() => {
+      element.style.opacity = 0;
+    }, 10);
+    setTransitionTimer(element, "opacity", duration, (element2) => {
+      element2.style.display = "none";
+      element2.style.opacity = "";
+      if (isFunction2(callback))
+        callback(element2);
+    });
+  }
+
   // modally.mjs
   var Modal = class {
     constructor(id, contentElement, options = {}, modallyInstance) {
       this.id = id;
       this.element = contentElement;
       this.modallyInstance = modallyInstance;
-      const landing = this.options.landing instanceof HTMLElement ? this.options.landing : document.querySelector(this.options.landing);
-      if (!landing)
-        return;
       this.videoRegEx = {};
       this.videoRegEx.YOUTUBE = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
       this.videoRegEx.VIMEO = /(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^\/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_\-]+)?/i;
@@ -131,6 +368,9 @@
         "out-css": null
         //TODO: css animation
       };
+      const landing = this.options.hasOwnProperty("landing") && this.options.landing instanceof HTMLElement ? this.options.landing : document.querySelector(this.options.landing);
+      if (!landing)
+        return;
       shallowMerge(this.options, options);
       if (this.element) {
         for (const k in this.options) {
@@ -141,7 +381,6 @@
       }
       this.template = parseDOM(this.options.template);
       this.template.setAttribute("modally-id", this.id);
-      this.element.setAttribute("modally-id", this.id);
       const modallyElement = this.template.querySelector(".modally");
       if (modallyElement) {
         css(modallyElement, {
@@ -154,7 +393,8 @@
           "verticalAlign": this.options["vertical-align"]
         });
       }
-      this.template.classList.add(this.options.classes);
+      if (!isEmpty(this.options.classes))
+        this.template.classList.add(this.options.classes);
       if (this.options.video)
         this.setupVideoLanding();
       else if (this.options.image)
@@ -167,7 +407,10 @@
       }
       if (this.element && this.element.classList.contains("modally-init")) {
         this.element.classList.remove("modally-init");
-        this.modallyInstance.open(this);
+      }
+      if (this.element) {
+        this.element.style.display = "";
+        this.element.removeAttribute("hidden");
       }
       this.template.querySelectorAll(".modally-close").forEach((el) => {
         el.addEventListener("click", (e) => {
@@ -175,7 +418,6 @@
           this.modallyInstance.close(this);
         });
       });
-      this.template.classList.add(id);
       landing.appendChild(this.template);
       this.zIndex = window.getComputedStyle(this.template).zIndex;
     }
@@ -185,9 +427,11 @@
       const vmod = this.options.autoplay ? "autoplay=1" : "";
       const vidmod = this.options.autoplay ? " autoplay" : "";
       const embeds = parseDOM(`
-      <iframe hidden class="youtube embed-template template" data-src="https://www.youtube.com/embed/{ID}?${ymod}autohide=1&amp;fs=1&amp;rel=0&amp;hd=1&amp;wmode=opaque&amp;enablejsapi=1" type="text/html" width="1920" height="1080" allow="autoplay" frameborder="0" vspace="0" hspace="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" scrolling="auto"></iframe>
-      <iframe hidden class="vimeo embed-template template" title="vimeo-player" data-src="https://player.vimeo.com/video/{ID}?${vmod}" type="text/html" width="1920" height="1080" allow="autoplay; allowfullscreen" rameborder="0" allowfullscreen=""></iframe>
-      <video hidden height="1920" width="1080" class="video embed-template template" data-src="{ID}" controls playsinline${vidmod}></video>
+      <div hidden>
+        <iframe hidden class="youtube embed-template template" data-src="https://www.youtube.com/embed/{ID}?${ymod}autohide=1&amp;fs=1&amp;rel=0&amp;hd=1&amp;wmode=opaque&amp;enablejsapi=1" type="text/html" width="1920" height="1080" allow="autoplay" frameborder="0" vspace="0" hspace="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" scrolling="auto"></iframe>
+        <iframe hidden class="vimeo embed-template template" title="vimeo-player" data-src="https://player.vimeo.com/video/{ID}?${vmod}" type="text/html" width="1920" height="1080" allow="autoplay; allowfullscreen" rameborder="0" allowfullscreen=""></iframe>
+        <video hidden height="1920" width="1080" class="video embed-template template" data-src="{ID}" controls playsinline${vidmod}></video>
+      </div>
     `);
       const landing = parseDOM('<div class="iframe-landing"></div>');
       landing.appendChild(spacer);
@@ -200,7 +444,61 @@
       this.template.querySelector(".modally-content").appendChild(spacer);
       this.template.classList.add("image-embed");
     }
-    open(callback) {
+    getVideoId(link) {
+      for (const k in this.videoRegEx) {
+        const match = link.match(this.videoRegEx[k]);
+        if (match)
+          return {
+            type: k.toLowerCase(),
+            id: match[1]
+          };
+      }
+      return false;
+    }
+    mountVideo(link) {
+      const vidData = this.getVideoId(link);
+      let template = this.template.querySelector(`.embed-template.template.${vidData.type}`);
+      const landing = this.template.querySelector(".modally-content .iframe-landing");
+      if (!template || !landing)
+        return;
+      template = template.cloneNode(true);
+      template.setAttribute("src", template.getAttribute("data-src").replace("{ID}", vidData.id));
+      template.style.display = "block";
+      template.removeAttribute("hidden");
+      landing.appendChild(template);
+    }
+    unmountVideo() {
+      const iframe = this.template.querySelector(".modally-content iframe, .modally-content video");
+      if (iframe)
+        iframe.remove();
+    }
+    mountImage(link) {
+      const img = this.template.querySelector(".modally-content img");
+      if (!img)
+        return;
+      img.setAttribute("src", link);
+      img.style.display = "block";
+      img.removeAttribute("hidden");
+    }
+    open(dataset, callback) {
+      if (this.options.video && dataset && dataset.hasOwnProperty("video")) {
+        this.mountVideo(dataset.video);
+      }
+      if (this.options.image && dataset && dataset.hasOwnProperty("image")) {
+        this.mountImage(dataset.image);
+      }
+      fadeIn(this.template, callback);
+    }
+    close(dataset, callback) {
+      fadeOut(this.template, () => {
+        if (this.options.video)
+          this.unmountVideo();
+        if (callback)
+          callback();
+        css(this.template, {
+          "zIndex": this.zIndex
+        });
+      });
     }
   };
   var Modally = class {
@@ -217,8 +515,8 @@
           if (modal) {
             e.preventDefault();
             if (target.matches('[target="_modal:close"]'))
-              return this.close(modal);
-            this.open(modal);
+              return this.close(modal, target.dataset);
+            this.open(modal, target.dataset);
           }
         }
       });
@@ -228,53 +526,49 @@
         }
       });
     }
-    modallyHashCheck() {
-      if (window.location.hash !== "" && window.location.hash !== "#") {
-        const href = window.location.hash.replace("#", "");
-        if (this.index.hasOwnProperty(href)) {
-          this.open(href);
+    modallyHashCheck(hash) {
+      const hashProperties = getHashProperties(hash);
+      for (const id in hashProperties) {
+        if (hashProperties[id] === void 0 && this.index.hasOwnProperty(id)) {
+          this.open(id, hashProperties);
         }
       }
     }
-    add(id, selector, options = {}) {
-      const element = isString(selector) ? document.querySelector(selector) : selector;
-      if (!element) {
-        console.error(`Modally: Element with selector "${selector}" not found`);
-        return;
-      }
+    add(id, options) {
+      let element = document.getElementById(id);
+      if (!options)
+        options = {};
+      if (!element && options.selector)
+        element = isString(options.selector) ? document.querySelector(options.selector) : options.selector;
       this.index[id] = new Modal(id, element, options, this);
     }
     get(id) {
       return this.index[id];
     }
-    open(id, callback) {
+    open(id, dataset, callback) {
       const modal = id instanceof Modal ? id : this.get(id);
       if (!modal)
         return;
-      modal.open(callback);
+      modal.open(dataset, callback);
       this.opened.push(modal);
       css(modal.template, {
         "zIndex": modal.zIndex + this.opened.length
       });
     }
-    close(id, callback) {
+    close(id, dataset, callback) {
       if (!id && this.opened.length) {
         id = this.opened[this.opened.length - 1];
       }
       const modal = id instanceof Modal ? id : this.get(id);
       if (!modal)
         return;
-      modal.close(callback);
+      modal.close(dataset, callback);
       this.opened.pop();
-      css(modal.template, {
-        "zIndex": modal.zIndex
-      });
     }
     // Only after registering all modals
     initHashCheck() {
-      this.modallyHashCheck();
-      window.addEventListener("hashchange", () => {
-        this.modallyHashCheck();
+      hashChange((hash) => {
+        this.modallyHashCheck(hash);
       });
     }
   };
